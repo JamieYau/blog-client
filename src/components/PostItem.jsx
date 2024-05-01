265496f07e04355bd38c7e960e40a4dd495e3d6c
JamieYau <jamie.yau2002@me.com>
2024-05-01 21:43:32 +0100

    Add truncated post content to postitem

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -3,17 +3,22 @@ import { Link } from "react-router-dom";
 import styles from "./PostItem.module.css";
 
 export default function PostItem({ post }) {
+  const formattedDate = new Date(post.createdAt).toLocaleDateString();
+  const truncatedContent =
+    post.content.length > 100
+      ? post.content.substring(0, 100) + "..."
+      : post.content;
+
   return (
     <li className={styles.postItem}>
       <Link to={`/posts/${post._id}`}>
         <h3 className={styles.postTitle}>{post.title}</h3>
+        <p className={styles.postContent}>{truncatedContent}</p>
         <p className={styles.postAuthor}>
           <span>by </span>
           {post.author}
         </p>
-        <p className={styles.postDate}>
-          {new Date(post.createdAt).toLocaleDateString()}
-        </p>
+        <p className={styles.postDate}>{formattedDate}</p>
       </Link>
     </li>
   );
@@ -24,6 +29,7 @@ PostItem.propTypes = {
   post: PropTypes.shape({
     _id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
     createdAt: PropTypes.string.isRequired,
   }).isRequired,
