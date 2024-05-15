2704c84bf48053be940e8293310d0f193f96b00a
JamieYau <jamie.yau2002@me.com>
2024-05-15 21:15:20 +0100

    Add logo to site

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,15 @@
 import styles from "./Nav.module.css"
 import { Link } from "react-router-dom";
+import logo from "../assets/logo1lightcrop.png"
 
 export default function Nav() {
   return (
     <nav className={styles.nav}>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <img className={styles.logo} src={logo} alt="<DevBlog/>" />
+          </Link>
         </li>
       </ul>
     </nav>
