7adde49ecf78552768ac395a1be840ff6097e7e2
JamieYau <jamie.yau2002@me.com>
2024-06-29 08:22:32 +0100

    Refactor search page to use useEffect to get posts whenever search params change

--- src/routes/SearchPage/SearchPage.tsx
+++ src/routes/SearchPage/SearchPage.tsx
@@ -1,7 +1,8 @@
 import { getPosts } from "@/api";
+import PostItem from "@/components/PostItem";
 import { Post } from "@/types/models";
 import { Search } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 export default function SearchPage() {
@@ -12,18 +13,24 @@ export default function SearchPage() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [recentSearches, setRecentSearches] = useState<string[]>([]);
 
+  // Fetch posts whenever searchParams change
+  useEffect(() => {
+    (async () => {
+      try {
+        const posts = searchParams.get("searchTerm")
+          ? await getPosts({ searchTerm: searchParams.get("searchTerm") || "" })
+          : await getPosts();
+        setPosts(posts);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    })();
+  }, [searchParams]);
+
   const handleSearchSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setSearchParams({ searchTerm: searchQuery });
     setRecentSearches((prev) => [...new Set([searchQuery, ...prev])]);
-    try {
-      const posts = searchQuery
-        ? await getPosts({ searchTerm: searchQuery })
-        : await getPosts();
-      setPosts(posts);
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-    }
   };
 
   return (
@@ -44,7 +51,7 @@ export default function SearchPage() {
       <section>
         {searchParams.get("searchTerm") ? (
           <>
-            <h1 className="my-7 text-2xl font-semibold tracking-tight text-muted-foreground">
+            <h1 className="my-7 text-2xl font-semibold tracking-tight text-muted-foreground sm:mt-0">
               Results for
               <span className="ml-1 text-foreground">
                 {searchParams.get("searchTerm")}
@@ -52,7 +59,7 @@ export default function SearchPage() {
             </h1>
             <ul>
               {posts.map((post) => (
-                <li key={post._id}>{post.title}</li>
+                <PostItem key={post._id} post={post} />
               ))}
             </ul>
           </>
