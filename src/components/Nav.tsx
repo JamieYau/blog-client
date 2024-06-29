aa8fd439ceacffd9068f76292a23c07182afbc94
JamieYau <jamie.yau2002@me.com>
2024-06-29 05:59:38 +0100

    Add navigate to search query url on submit

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useAuth from "@/contexts/useAuth";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { buttonVariants } from "@/components/ui/button";
@@ -12,6 +12,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { LogOut, Search } from "lucide-react";
+import { useState } from "react";
 const bigLogo = "/logo1crop.png";
 const logo = "/logo2crop.png";
 
@@ -19,6 +20,13 @@ export default function Nav() {
   const { user, logout } = useAuth();
   const isLoggedIn = user !== null;
   const userInitial = isLoggedIn ? user.username.charAt(0).toUpperCase() : "";
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate(`/search?query=${searchQuery}`);
+  };
 
   return (
     <header className="flex w-full justify-center shadow-sm">
@@ -32,12 +40,17 @@ export default function Nav() {
           <img src={logo} alt="</>" className="h-10 sm:hidden" />
         </Link>
         <div className="flex w-full items-center justify-end gap-6 sm:justify-between">
-          <form className="ml-4 hidden w-64 items-center rounded-full bg-muted sm:flex">
+          <form
+            onSubmit={handleSearchSubmit}
+            className="ml-4 hidden w-64 items-center rounded-full bg-muted sm:flex"
+          >
             <Search className="mx-3 min-h-6 min-w-6 stroke-[1.5] text-muted-foreground" />
             <input
               type="text"
               placeholder="Search"
               className="w-full rounded border border-none bg-transparent px-5 py-[10px] pl-0 outline-none placeholder:text-muted-foreground"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
             />
           </form>
           <Link to="/search" className="sm:hidden">
