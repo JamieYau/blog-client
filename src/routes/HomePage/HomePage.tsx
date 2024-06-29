1b16d25bd7ed7e3946c7f7cb681b8d8e2f7517f6
JamieYau <jamie.yau2002@me.com>
2024-06-29 03:31:36 +0100

    Remove redundant posts sort

--- src/routes/HomePage/HomePage.tsx
+++ src/routes/HomePage/HomePage.tsx
@@ -8,9 +8,6 @@ export default function HomePage() {
     // Handle case where posts is not yet loaded or is null
     return null; // or loading indicator or error message
   }
-  posts.sort(
-    (a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime(),
-  );
   return (
     <section className="flex flex-col">
       <h1 className="mb-10 w-full border-b pb-8 text-center text-4xl font-semibold">
