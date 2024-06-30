49f11465bc1ef235514fc53f4b231e38db5a865f
JamieYau <jamie.yau2002@me.com>
2024-06-30 21:58:13 +0100

    Add grid layout at md screen size, with new postitem format

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -11,32 +11,33 @@ export default function PostItem({ post }: PostItemProps) {
   const formattedDate = new Date(post.createdAt).toLocaleDateString();
 
   return (
-    <li className="border-b pb-4">
-      <Link
-        to={`/posts/${post._id}`}
-        className="flex flex-col gap-4 md:flex-row"
-      >
+    <li className="pb-4">
+      <Link to={`/posts/${post._id}`} className="flex flex-col gap-4">
         <img
           src={post.coverImageUrl || placeholder}
-          className="aspect-[4/3] w-full md:max-w-lg object-cover"
+          className="aspect-[5/3] w-full object-cover md:aspect-[4/3]"
         />
         <div className="flex min-h-full w-full flex-col text-muted-foreground">
-          <h2 className="mb-2 text-2xl font-bold text-foreground">
-            {post.title}
-          </h2>
-          <p className="color-muted-foreground mb-1 text-sm">{formattedDate}</p>
-          <span className="mb-4 space-x-2">
-            {post.tags.map((tag) => (
-              <Badge key={tag}>{tag}</Badge>
-            ))}
-          </span>
-          <p className="mb-1 line-clamp-4 tracking-tight">
-            {stripHtmlTags(post.content)}
-          </p>
-          <p className="flex w-full flex-1 items-end justify-end font-medium leading-none gap-1">
+          <p className="my-4 flex w-full flex-1 items-end gap-1 font-medium leading-none">
             <span>by </span>
             {post.author}
           </p>
+          <h2 className="mb-2 text-2xl font-bold text-foreground">
+            {post.title}
+          </h2>
+          <p className="mb-4 line-clamp-2 tracking-tight">
+            {stripHtmlTags(post.content)}
+          </p>
+          <div className="flex justify-between">
+            <p className="color-muted-foreground mb-1 text-sm">
+              {formattedDate}
+            </p>
+            <span className="mb-4 space-x-2">
+              {post.tags.map((tag) => (
+                <Badge key={tag}>{tag}</Badge>
+              ))}
+            </span>
+          </div>
         </div>
       </Link>
     </li>
